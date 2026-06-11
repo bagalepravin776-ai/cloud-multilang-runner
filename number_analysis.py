1de19df6382ec90d5f14843914f96d028b05dde2
bagalepravin776-ai <bagalepravin776@gmail.com>
2026-06-11 11:38:58 +0530

    Create number_analysis.py

---
+++ number_analysis.py
@@ -0,0 +1,9 @@
+numbers = [12, 45, 67, 23, 89, 34, 56, 78, 90, 11]
+
+highest = max(numbers)
+lowest = min(numbers)
+average = sum(numbers) / len(numbers)
+
+print("Highest Number:", highest)
+print("Lowest Number:", lowest)
+print("Average:", average)
